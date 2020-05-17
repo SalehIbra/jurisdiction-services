f17c98696dff7d8c548f041b990585e9ab77b81f
= <=>
2020-05-18 00:08:04 +0200

    - Create entities for Jurisdiction service JPA

---
+++ src/main/java/com/nolimit/demo/entity/Configuration.java
@@ -0,0 +1,75 @@
+package com.nolimit.demo.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "configuration")
+public class Configuration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "configuration_id")
+    private Long id;
+
+    @Column(name = "key")
+    private String key;
+
+    @Column(name = "definition")
+    private String definition;
+
+    @OneToMany(mappedBy = "configuration",cascade = CascadeType.ALL)
+    private List<JurisdictionConfig> jurisdictions = new ArrayList<>();
+
+    public Configuration() {
+    }
+
+    public Configuration(String key, String definition) {
+        this.key = key;
+        this.definition = definition;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public List<JurisdictionConfig> getJurisdictions() {
+        return jurisdictions;
+    }
+
+    public void setJurisdictions(List<JurisdictionConfig> jurisdictions) {
+        this.jurisdictions = jurisdictions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Configuration that = (Configuration) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
---
+++ src/main/java/com/nolimit/demo/entity/Jurisdiction.java
@@ -0,0 +1,77 @@
+package com.nolimit.demo.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "jurisdiction")
+public class Jurisdiction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="jurisdiction_id")
+    private Long id;
+
+    @NotNull
+    @Column(name="jurisdiction_code")
+    private String code;
+
+    @Column(name="jurisdiction_name")
+    private String name;
+
+    @OneToMany(mappedBy = "jurisdiction",cascade = CascadeType.ALL)
+    private List<JurisdictionConfig> configurations = new ArrayList<>();
+
+    public Jurisdiction() {
+    }
+
+    public Jurisdiction(@NotNull String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<JurisdictionConfig> getConfigurations() {
+        return configurations;
+    }
+
+    public void setConfigurations(List<JurisdictionConfig> configurations) {
+        this.configurations = configurations;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Jurisdiction that = (Jurisdiction) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
